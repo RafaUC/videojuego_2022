945d8c41be90284d4faca608a95c49ccf2b9c70b
Rafael Uribe Caldera <35164744@uaz.edu.mx>
2022-11-15 03:07:09 -0600

    se completo el fondo

--- Videojuego_2022/Assets/World/Tiles/Parallax.cs
+++ Videojuego_2022/Assets/World/Tiles/Parallax.cs
@@ -14,34 +14,52 @@ public class Parallax : MonoBehaviour
     public Vector2 originalSize;
     public Vector2 size => GetComponent<SpriteRenderer>().size;
 
-    float startZ;
+    public Vector2 ofSet;
+    public Vector2 pivotOfSet;
 
-    Vector2 travel => (Vector2)cam.transform.position - startPos;
+    public float startZ;
+
+    Vector2 travel;
     float distanceFromsubject => transform.position.z - subject.position.z;
     float clippingPlane => (cam.transform.position.z + (distanceFromsubject > 0? cam.m_Lens.FarClipPlane : cam.m_Lens.NearClipPlane));
 
     float parallaxFactor => Mathf.Abs(distanceFromsubject) / clippingPlane;
     
+    public float parallasx;
+    
 
     public void Start(){
         originalSize = GetComponent<SpriteRenderer>().size;
         if(xInfinitetiling){
-            GetComponent<SpriteRenderer>().size = new Vector2(originalSize.x*3,originalSize.y);
+            GetComponent<SpriteRenderer>().size = new Vector2(originalSize.x*2,originalSize.y);
+            pivotOfSet = new Vector2(size.x/2,size.y/2);
         }
+        
         startPos = transform.position;
+        
+        ofSet = (Vector2)cam.transform.position - startPos;
         startZ = transform.position.z;
     }
 
+    
+
     public void Update(){
-        
+        parallasx = parallaxFactor;
+        travel = (Vector2)cam.transform.position - startPos - ofSet;
+
         Vector2 newPos = startPos + (travel * parallaxFactor);
         transform.position = new Vector3(newPos.x, newPos.y, startZ);
 
         if(xInfinitetiling){
-            if(Mathf.Abs(travel.x) > size.x/6*2){
-                startPos = new Vector2(transform.position.x+(size.x/3*Mathf.Sign(travel.x)), startPos.y);
+            if(cam.transform.position.x - (transform.position.x + pivotOfSet.x) > size.x/4){
+                startPos = new Vector2(transform.position.x+(size.x/2), startPos.y);
+                ofSet.x = ((Vector2)cam.transform.position).x - startPos.x;
                 transform.position = new Vector3(startPos.x,transform.position.y,transform.position.z);
                 //startPos = transform.position;
+            }else if(cam.transform.position.x - (transform.position.x + pivotOfSet.x) < -size.x/4){
+                startPos = new Vector2(transform.position.x-(size.x/2), startPos.y);
+                ofSet.x = ((Vector2)cam.transform.position).x - startPos.x;
+                transform.position = new Vector3(startPos.x,transform.position.y,transform.position.z);
             }
         }
     }
