880ddeb92cb9adf7c0db06af72805c66a6f3793f
EsmeraldaPachecoGlez <esmeraldapacheco05@gmail.com>
2022-11-18 14:49:44 -0600

    Se agregó la función QuitGame() en SceneSwitcher, la cual hace que una vez se haga el build del videojugo, si le damos click al botón de Exitse cerrará el programa

--- Videojuego_2022/Assets/World/Tiles/SceneSwitcher.cs
+++ Videojuego_2022/Assets/World/Tiles/SceneSwitcher.cs
@@ -9,4 +9,10 @@ public class SceneSwitcher : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame() //Por ahora no hace nada, pero cuando importemos el videojuego si va a cerrar el programa
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
 }
